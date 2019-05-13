9998d1786b62b75cb5d8b1bc4cf10ee177c4eed9
Moritz Sternemann <moritz@strnmn.me>
2019-05-13 10:46:39 +0200

    feat: Added TeamMember model

---
+++ src/model/TeamMember.ts
@@ -0,0 +1,11 @@
+import { Field, Record, CloudKit } from '.'
+
+export default class TeamMember extends Record {
+  static recordType = 'TeamMember'
+
+  @Field public name!: string
+  @Field public birthday!: number
+  @Field public biography!: string
+  @Field public picture!: CloudKit.Asset
+  @Field public scholar!: CloudKit.Reference
+}
