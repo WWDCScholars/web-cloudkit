c30eb19b73990422b123a77e8b817bfe5cca398c
Moritz Sternemann <moritz@strnmn.me>
2019-05-30 15:16:30 +0200

    fix: Scholar ref in ScholarPrivate is non-null

--- src/model/ScholarPrivate.ts
+++ src/model/ScholarPrivate.ts
@@ -2,8 +2,8 @@ import { CloudKit, Record, Field } from '.'
 
 export default class ScholarPrivate extends Record {
   static recordType = 'ScholarPrivate'
-  
+
   @Field public birthday!: number
   @Field public email!: string
-  @Field public scholar?: CloudKit.Reference
+  @Field public scholar!: CloudKit.Reference
 }
